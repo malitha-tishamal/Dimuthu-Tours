b4ea48ac2565742570e4b092c4cf71203f132f56
malitha-tishamal <malithatishamal@gmail.com>
2026-03-20 16:53:17 +0530

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -22,6 +22,9 @@ require_once __DIR__ . '/db.php';
     <!-- FontAwesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     
+    <!-- Bootstrap Icons -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
+    
     <!-- Custom CSS -->
     <link rel="stylesheet" href="index.css">
 </head>
